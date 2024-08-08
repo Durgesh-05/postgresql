fbd731364766c9e7faa014a716d59c2153615ded
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-08-08 17:10:43 +0530

    insert and create complete

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,53 @@
-console.log('Welcome to sql db learnig');
+import { Client } from 'pg';
+import { PostgresConfig } from './config';
+
+const client = new Client({
+  connectionString: PostgresConfig.ConnectionString,
+});
+
+(async function () {
+  await client.connect();
+})();
+
+// Create a fn which create table users
+async function createUsersTable() {
+  await client.connect();
+  const result = await client.query(`
+    CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+`);
+  console.log(result);
+}
+
+// Create a fn which insert data into users
+async function insertUsersTable() {
+  // To avoid sql injection
+  const query =
+    'INSERT INTO users(username, email, password) VALUES($1, $2, $3);';
+  const values = ['durgesh18', 'durgesh@gmail.com', '12345678'];
+  const result = await client.query(query, values);
+  console.log(result);
+}
+
+// Create a fn to find all users
+async function getAllUsers() {
+  const result = await client.query('SELECT * FROM users');
+  console.log(result);
+}
+
+// Create a fn to find particular user
+async function getSelectedUser(username: string) {
+  const query = 'SELECT * FROM users WHERE username = $1';
+  const result = await client.query(query, [username]);
+  console.log(result.rows);
+}
+
+// createUsersTable();
+// insertUsersTable();
+// getAllUsers();
+getSelectedUser('dragon18');
